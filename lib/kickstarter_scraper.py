62c9eca347297dbcf88854c1d4555b1c9c4708da
Agent-grey <markkibuthu@gmail.com>
2024-10-15 19:43:26 +0300

    complete lab

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,31 +1,38 @@
+# file: kickstarter_scraper.py
 from bs4 import BeautifulSoup
 import ipdb
-
-# projects: kickstarter.select("li.project.grid_4")[0]
-# title: project.select("h2.bbcard_name strong a")[0].text
-# image link: project.select("div.project-thumbnail a img")[0]['src']
-# description: project.select("p.bbcard_blurb")[0].text
-# location: project.select("ul.project-meta span.location-name")[0].text
-# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
 
 def create_project_dict():
     html = ''
     with open('./fixtures/kickstarter.html') as file:
         html = file.read()
+
     kickstarter = BeautifulSoup(html, 'html.parser')
     projects = {}
+
     # Iterate through the projects
     for project in kickstarter.select("li.project.grid_4"):
-        title = project.select("h2.bbcard_name strong a")[0].text
+        title_element = project.select("h2.bbcard_name strong a")
+        title = title_element[0].text if title_element else "No Title"
+        
+        image_element = project.select("div.project-thumbnail a img")
+        image_link = image_element[0]['src'] if image_element else "No Image"
+
+        description_element = project.select("p.bbcard_blurb")
+        description = description_element[0].text if description_element else "No Description"
+
+        location_element = project.select("ul.project-meta span.location-name")
+        location = location_element[0].text if location_element else "No Location"
+
+        percent_funded_element = project.select("ul.project-stats li.first.funded strong")
+        percent_funded = percent_funded_element[0].text.replace("%", "") if percent_funded_element else "0"
+
         projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
+            'image_link': image_link,
+            'description': description,
+            'location': location,
+            'percent_funded': percent_funded
         }
-    # return the projects dictionary
 
+    # Return the projects dictionary
     return projects
-
-projects = create_project_dict()
-print(projects)
